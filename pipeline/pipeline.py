019399f77d4051786ecbbf009f47520db4d9af99
acer <acer@mac.home>
2025-08-20 19:57:51 -0400

    fixed pipeline.py

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -7,33 +7,30 @@ from utils.custom_exception import CustomException
 logger = get_logger(__name__)
 
 class AnimeRecommendationPipeline:
-    def __init__(self,persist_directory:str="chroma_db"):
+    def __init__(self,persist_dir="chroma_db"):
         try:
-            logger.info("Initializing Anime Recommendation Pipeline")
-            vector_builder = VectorStoreBuilder(csv_path="data/anime_with_synopsis.csv", persist_directory=persist_directory)
-            
-            # Try to load existing vector store, build if it doesn't exist
-            try:
-                retriever = vector_builder.load_vector_store().as_retriever()
-                logger.info("Loaded existing vector store")
-            except:
-                logger.info("Building new vector store...")
-                vector_builder.build_and_save_vector_store()
-                retriever = vector_builder.load_vector_store().as_retriever()
-                logger.info("Vector store built successfully")
-            
+            logger.info("Intializing Recommdation Pipeline")
+
+            vector_builder = VectorStoreBuilder(csv_path="" , persist_dir=persist_dir)
+
+            retriever = vector_builder.load_vector_store().as_retriever()
+
             self.recommender = AnimeRecommender(retriever,GROQ_API_KEY,MODEL_NAME)
-            logger.info("Anime Recommendation Pipeline initialized successfully")
+
+            logger.info("Pipleine intialized sucesfully...")
+
         except Exception as e:
-            logger.error(f"Error initializing Anime Recommendation Pipeline: {str(e)}")
-            raise CustomException("Error initializing Anime Recommendation Pipeline",e)
+            logger.error(f"Failed to intialize pipeline {str(e)}")
+            raise CustomException("Error during pipeline intialization" , e)
         
     def recommend(self,query:str) -> str:
         try:
-            logger.info(f"Getting recommendation for query: {query}")
+            logger.info(f"Recived a query {query}")
+
             recommendation = self.recommender.get_recommendation(query)
-            logger.info(f"Recommendation: {recommendation}")
+
+            logger.info("Recommendation generated successfully...")
             return recommendation
         except Exception as e:
-            logger.error(f"Error getting recommendation: {str(e)}")
-            raise CustomException("Error getting recommendation",e)
+            logger.error(f"Failed to get recommendation {str(e)}")
+            raise CustomException("Error during getting recommendation" , e)
